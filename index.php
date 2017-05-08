d4774a33b666ee1980062ec6a217de405b10252a
Letitbe133 <lionel.lft@me.com>
2017-05-08 18:50:31 +0200

    Start structure code

--- index.php
+++ index.php
@@ -10,7 +10,51 @@
     <link rel="stylesheet" href="css/master.css">
   </head>
   <body>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons md-36">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li class="active"><a href="#!">About</a></li>
+          <li><a href="#!">Activities</a></li>
+          <li><a href="#!">Products</a></li>
+          <li><a href="#!">Contact</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-menu">
+        <li><a href="#!">About</a></li>
+        <li><a href="#!">Activities</a></li>
+        <li><a href="#!">Products</a></li>
+        <li><a href="#!">Contact</a></li>
+      </ul>
+      </div>
+    </nav>
+  </div>
+  <div class="row">
+    <div class="col s12">
+      <h1>We care for you</h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+    </div>
+  </div>
+  <main>
+    <section class="row">
+      <div class="col s12">
 
+      </div>
+      <div class="col s12">
+
+      </div>
+    </section>
+  </main>
+    <!-- <script type="text/javascript">
+      /* First CSS File */
+      var giftofspeed = document.createElement('link');
+      giftofspeed.rel = 'stylesheet';
+      giftofspeed.href = 'css/master.css';
+      giftofspeed.type = 'text/css';
+      var godefer = document.getElementsByTagName('link')[0];
+      godefer.parentNode.insertBefore(giftofspeed, godefer);
+    </script> -->
     <script
 			  src="https://code.jquery.com/jquery-3.2.1.min.js"
 			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
