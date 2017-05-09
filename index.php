24a24cb53122f47ffc6a84ea69ef3803fba4ba53
Letitbe133 <lionel.lft@me.com>
2017-05-09 19:02:07 +0200

    Added desktop structure

--- index.php
+++ index.php
@@ -10,193 +10,222 @@
     <link rel="stylesheet" href="css/master.css">
   </head>
   <body>
-  <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo">Logo</a>
-        <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons md-36">menu</i></a>
-        <ul class="right hide-on-med-and-down">
-          <li class="active"><a href="#!">About</a></li>
-          <li><a href="#!">Activities</a></li>
-          <li><a href="#!">Products</a></li>
-          <li><a href="#!">Contact</a></li>
-        </ul>
-        <ul class="side-nav" id="mobile-menu">
-        <li><a href="#!">About</a></li>
-        <li><a href="#!">Activities</a></li>
-        <li><a href="#!">Products</a></li>
-        <li><a href="#!">Contact</a></li>
-      </ul>
+    <header class="row">
+      <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#!" class="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons md-36">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+              <li class="active"><a href="#!">About</a></li>
+              <li><a href="#!">Activities</a></li>
+              <li><a href="#!">Products</a></li>
+              <li><a href="#!">Contact</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-menu">
+            <li><a href="#!">About</a></li>
+            <li><a href="#!">Activities</a></li>
+            <li><a href="#!">Products</a></li>
+            <li><a href="#!">Contact</a></li>
+          </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
-  </div>
-  <div class="row">
-    <div class="col s12">
-      <h1>We care for you</h1>
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
-    </div>
-  </div>
-  <main>
-    <section class="row">
-      <div class="col s12">
-        <h2 class="green-text">About us</h2>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div class="col s12">
-
-      </div>
-    </section>
-    <section class="row">
-      <div class="col s12">
-        <h2 class="green-text">Activities</h2>
-      </div>
-      <div class="col s4 offset-s4">
-        Picto 1
-      </div>
-      <div class="col s8 offset-s2">
-        <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-      </div>
-      <div class="col s4 offset-s4">
-        Picto 2
-      </div>
-      <div class="col s8 offset-s2">
-        <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-      </div>
-      <div class="col s4 offset-s4">
-        Picto 3
-      </div>
-      <div class="col s8 offset-s2">
-        <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-      </div>
-      <div class="col s12">
-        Image shake hands
-      </div>
-      <div class="col s12">
-        Picto monde
-      </div>
-      <div class="col s1 offset-s1">
-        <i class="material-icons">location_on</i>
-      </div>
-      <div class="col s9">
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-      </div>
-      <div class="col s1 offset-s1">
-        <i class="material-icons">location_on</i>
-      </div>
-      <div class="col s9">
-        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
-      </div>
-    </section>
-    <section class="row">
-      <div class="col s12">
-        <h2 class="green-text">Products</h2>
-      </div>
-      <form class="col s10 offset-s1">
-        <div class="input-field">
-          <input id="search" type="search" required>
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-          <i class="material-icons">close</i>
+      <div class="row">
+        <div class="col s12">
+          <h1>We care for you</h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
         </div>
+        <div class="col s2 offset-s5">
+          <i class="material-icons md-36">arrow_downward</i>
+        </div>
+      </div>
+    </header>
+    <main>
+      <section class="row">
+        <div class="col s12 l8">
+          <h2 class="green-text">About us</h2>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col s12 l4">
+          Image section About us
+        </div>
+      </section>
+      <section class="row grey lighten-4">
+        <div class="col s12">
+          <h2 class="green-text">Activities</h2>
+        </div>
+        <div class="col s12 l4">
+          <div class="col s4 offset-s4">
+            Picto 1
+          </div>
+          <div class="col s8 offset-s2">
+            <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          </div>
+        </div>
+        <div class="col s12 l4">
+          <div class="col s4 offset-s4">
+            Picto 2
+          </div>
+          <div class="col s8 offset-s2">
+            <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          </div>
+        </div>
+        <div class="col s12 l4">
+          <div class="col s4 offset-s4">
+            Picto 3
+          </div>
+          <div class="col s8 offset-s2">
+            <h3 class="align-center">Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          </div>
+        </div>
+        <div class="col s12">
+          Image shake hands
+        </div>
+        <div class="col s12 l6">
+          Picto monde
+        </div>
+        <div class="col l6">
+          <div class="col s1 offset-s1">
+            <i class="material-icons md-36">location_on</i>
+          </div>
+          <div class="col s9">
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          </div>
+          <div class="col s1 offset-s1">
+            <i class="material-icons md-36">location_on</i>
+          </div>
+          <div class="col s9">
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+          </div>
+        </div>
+      </section>
+      <section class="row grey lighten-4">
+        <div class="col s12 l6">
+          <h2 class="green-text">Products</h2>
+        </div>
+        <form class="col s10 offset-s1 l6">
+          <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+        <div class="col s10 offset-s1">
+          <div class="carousel">
+            <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/250/250/nature/1"></a>
+            <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/250/250/nature/2"></a>
+            <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/3"></a>
+            <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/4"></a>
+            <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
+          </div>
+        </div>
+      </section>
+      <section class="row">
+        <div class="col s12">
+          <h2 class="green-text">Partners</h2>
+        </div>
+        <div class="col l12 hide-on-small-only">
+          <h3>Lorem ipsum dolor sit amet</h3>
+        </div>
+        <div class="col l12 hide-on-small-only">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col s6">
+          <div class="col s10 offset-s1">
+            Partner 1
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="col s10 offset-s1">
+            Partner 2
+          </div>
+        </div>
+        <div class="col s1 offset-s1">
+          <i class="material-icons md-36">check_circle</i>
+        </div>
+        <div class="col s9">
+          <h2 class="col s12">GMP</h3>
+          <h3 class="col s12">Good Manufacturing Practice</h2>
+        </div>
+        <div class="col s1 offset-s1">
+          <i class="material-icons md-36">check_circle</i>
+        </div>
+        <div class="col s9">
+          <h2 class="col s12">GDP</h3>
+          <h3 class="col s12">Good Distribution Practice</h2>
+        </div>
+        <div class="col s12 hide-on-med-and-up">
+          Image feuille
+        </div>
+      </section>
+      <section class="row grey lighten-4">
+        <div class="col s12">
+          <h2 class="green-text">Contact</h2>
+        </div>
+        <form class="col s10 offset-s1 l8">
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix md-36">account_circle</i>
+              <input placeholder="John Doe" id="name" type="text" class="validate">
+              <label for="name">Your name</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix md-36">email</i>
+              <input placeholder="john.doe@mail.com" id="email"type="email" class="validate">
+              <label for="email">Your email</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix md-36">subject</i>
+              <input id="subject" type="text" class="validate">
+              <label for="subject">Subject</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix md-36">textsms</i>
+              <textarea id="message" type="email" class="materialize-textarea"></textarea>
+              <label for="message">Your message</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s12 l3 offset-l9 center-align">
+              <button class="btn waves-effect waves-light" type="submit" name="submit">Send message
+                <i class="material-icons right">send</i>
+              </button>
+            </div>
+          </div>
       </form>
-      <div class="col s10 offset-s1">
-        <div class="carousel">
-          <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/250/250/nature/1"></a>
-          <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/250/250/nature/2"></a>
-          <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/3"></a>
-          <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/4"></a>
-          <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
-        </div>
+      <div class="col l4 hide-on-small-only">
+        Image travail
       </div>
-    </section>
-    <section class="row">
+      </section>
+    </main>
+    <footer class="row grey darken-3">
       <div class="col s12">
-        <h2 class="green-text">Partners</h2>
+        <h2>Head Office</h2>
+        <address class="col s12 l4">
+          MEDILAB UK Ltd<br/>
+          41 Chalton Street, London, United Kingdom, NW1 1jD
+        </address>
+        <address class="col s12 l4">
+          Phone : +44 20 7554 2222<br/>
+          Fax : +44 20 7554 2201
+        </address>
+        <address class="col s12 l4">
+          Mail : <a href="mailto:medilab@medilab.one">medilab@medliab.one</a>
+        </address>
       </div>
-      <div class="col s6">
-        <div class="col s10 offset-s1">
-          Partner 1
-        </div>
+      <div class="divider"></div>
+      <div class="col s12 center-align">
+        &copy&nbspCopyright 2017. All rights reserved. Legal informations. Terms of use
       </div>
-      <div class="col s6">
-        <div class="col s10 offset-s1">
-          Partner 2
-        </div>
-      </div>
-      <div class="col s1 offset-s1">
-        <i class="material-icons">check_circle</i>
-      </div>
-      <div class="col s9">
-        <h2 class="col s12">GMP</h3>
-        <h3 class="col s12">Good Manufacturing Practice</h2>
-      </div>
-      <div class="col s1 offset-s1">
-        <i class="material-icons">check_circle</i>
-      </div>
-      <div class="col s9">
-        <h2 class="col s12">GDP</h3>
-        <h3 class="col s12">Good Distribution Practice</h2>
-      </div>
-      <div class="col s12">
-        Image feuille
-      </div>
-    </section>
-    <section class="row">
-      <div class="col s12">
-        <h2 class="green-text">Contact</h2>
-      </div>
-      <form class="col s10 offset-s1">
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">account_circle</i>
-            <input placeholder="John Doe" id="name" type="text" class="validate">
-            <label for="name">Your name</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">email</i>
-            <input placeholder="john.doe@mail.com" id="email"type="email" class="validate">
-            <label for="email">Your email</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">subject</i>
-            <input id="subject" type="text" class="validate">
-            <label for="subject">Subject</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">textsms</i>
-            <textarea id="message" type="email" class="materialize-textarea"></textarea>
-            <label for="message">Your message</label>
-          </div>
-        </div>
-    </form>
-    </section>
-  </main>
-  <footer class="row">
-    <div class="col s12">
-      <h2>Head Office</h2>
-      <address class="col s12">
-        MEDILAB UK Ltd<br/>
-        41 Chalton Street, London, United Kingdom, NW1 1jD
-      </address>
-      <address class="col s12">
-        Phone : +44 20 7554 2222<br/>
-        Fax : +44 20 7554 2201
-      </address>
-      <address class="col s12">
-        Mail : <a href="mailto:medilab@medilab.one">medilab@medliab.one</a>
-      </address>
-    </div>
-    <div class="divider"></div>
-    <div class="col s12 center-align">
-      &copy&nbspCopyright 2017. All rights reserved. Legal informations. Terms of use
-    </div>
-  </footer>
+    </footer>
     <!-- <script type="text/javascript">
       /* First CSS File */
       var giftofspeed = document.createElement('link');
