ea9110897a32fcd4b6742f0b41c9e82df4d8536d
Letitbe133 <lionel.lft@me.com>
2017-07-10 12:56:06 +0200

    Deleted Materialize sliders

--- index.php
+++ index.php
@@ -211,42 +211,8 @@
           <div class="slide">
             <img src="assets/img/produits/clomed-pommade.png" alt="" width="300px">
           </div>
-          <!--<div class="slide">
-            <img src="assets/img/produits/gentamed.png" alt="" width="300px">
-          </div>
-          <div class="slide">
-            <img src="assets/img/produits/clomed-pommade.png" alt="" width="300px">
-          </div>-->
         </div>
       </div>
-      <!-- <div class="row">
-        <div class="col s10 offset-s1 l4">
-          <img class="responsive-img" src="assets/img/produits/gentamed.png" alt="Gentamed">
-        </div>
-        <div class="col s10 offset-s1 l4">
-          <img class="responsive-img" src="assets/img/produits/betamed-pommade.png" alt="Betamed pommade">
-        </div>
-        <div class="col s10 offset-s1 l4">
-          <img class="responsive-img" src="assets/img/produits/parafene.png" alt="Parafen">
-        </div>
-      </div> -->
-      <!-- <div class="col s10 offset-s1 hide-on-small-only">
-        <div class="carousel carousel-slider" data-indicators="true">
-          <a class="carousel-item" href="#one!"><img src="assets/img/produits/gentamed.png" alt="Gentamed" width="50%"></a>
-          <a class="carousel-item" href="#two!"><img src="assets/img/produits/betamed-pommade.png" alt="Betamed pommade"></a>
-          <a class="carousel-item" href="#three!"><img src="assets/img/produits/parafene.png" alt="Parafen"></a>
-          <a class="carousel-item" href="#four!"><img src="assets/img/produits/beta-salic-pommade.png" alt="Beta Salic"></a>
-        </div>
-      </div>
-      <div class="col s10 offset-s1 hide-on-med-and-up">
-        <div class="carousel" data-indicators="true">
-          <a class="carousel-item" href="#one!"><img src="assets/img/produits/gentamed.png" alt="Gentamed"></a>
-          <a class="carousel-item" href="#two!"><img src="assets/img/produits/betamed-pommade.png" alt="Betamed pommade"></a>
-          <a class="carousel-item" href="#three!"><img src="assets/img/produits/parafene.png" alt="Parafen"></a>
-          <a class="carousel-item" href="#four!"><img src="assets/img/produits/beta-salic-pommade.png" alt="Beta Salic"></a>
-        </div>
-      </div> -->
-
       <section id="partners" class="section row scrollspy no-margin-bottom">
         <div class="row">
           <div class="col s12">
